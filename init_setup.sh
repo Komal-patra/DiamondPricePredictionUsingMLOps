7f4f451269498c874ac6da74beb2d13afb3f339f
Komal-patra <komalfsds 2022@gmail.com>
2024-05-04 20:45:22 +0100

    src component folder created

--- init_setup.sh
+++ init_setup.sh
@@ -9,7 +9,7 @@ conda create -n diamondml python=3.8 -y
 
 echo [$(date)]: "activating the environment" 
 
-conda activate diamondml
+source activate diamondml
 
 echo [$(date)]: "installing the dev requirements" 
 
