0720e7fcd3532cb967ad44d3cd0373dcf1992ae5
Komal-patra <komalfsds 2022@gmail.com>
2024-05-06 11:47:06 +0100

    Template modified

--- GemstonePricePrediction/pipelines/prediction_pipeline.py
+++ GemstonePricePrediction/pipelines/prediction_pipeline.py
@@ -27,9 +27,43 @@ class PredictPipeline:
             raise CustomException(e,sys)
         
 class CustomData:
-
-    def __init__(self):
-        pass
-
+    def __init__(self,
+                 carat:float,
+                 depth:float,
+                 table:float,
+                 x:float,
+                 y:float,
+                 z:float,
+                 cut:str,
+                 color:str,
+                 clarity:str):
+        
+        self.carat=carat
+        self.depth=depth
+        self.table=table
+        self.x=x
+        self.y=y
+        self.z=z
+        self.cut = cut
+        self.color = color
+        self.clarity = clarity
+            
     def get_data_as_dataframe(self):
-        pass
+        try:
+            custom_data_input_dict = {
+                'carat':[self.carat],
+                'depth':[self.depth],
+                'table':[self.table],
+                'x':[self.x],
+                'y':[self.y],
+                'z':[self.z],
+                'cut':[self.cut],
+                'color':[self.color],
+                'clarity':[self.clarity]
+                }
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('Dataframe Gathered')
+            return df
+        except Exception as e:
+            logging.info('Exception Occured in prediction pipeline')
+            raise CustomException(e,sys)
--- app.py
+++ app.py
@@ -1,41 +1,45 @@
-from flask import Flask,request,render_template
+# app.py
 
-from GemstonePricePrediction.pipelines.prediction_pipeline import PredictPipeline,CustomData
+from flask import Flask, request, render_template, jsonify
+from GemstonePricePrediction.pipelines.prediction_pipeline import PredictPipeline, CustomData
+from GemstonePricePrediction.components.model_trainer import ModelTrainer
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home_page():
     return render_template("index.html")
 
-@app.route("/predict",methods=["GET","POST"])
+
+@app.route("/predict", methods=["GET", "POST"])
 def predict_datapoint():
-    if request.method=="GET":
+    """
+    Prediction method
+    """
+    if request.method == "GET":
         return render_template("form.html")
+    
     else:
-        data=CustomData(
-            carat=float(request.form.get("carat")),
-            depth=float(request.form.get("depth")),
-            table=float(request.form.get("table")),
-            x=float(request.form.get("x")),
-            y=float(request.form.get("y")),
-            z=float(request.form.get("z")),
-            cut=request.form.get("cut"),
-            color=request.form.get("color"),
-            clarity=request.form.get("clarity")
+        data = CustomData(
+            
+            carat=float(request.form.get('carat')),
+            depth=float(request.form.get('depth')),
+            table=float(request.form.get('table')),
+            x=float(request.form.get('x')),
+            y=float(request.form.get('y')),
+            z=float(request.form.get('z')),
+            cut=request.form.get('cut'),
+            color=request.form.get('color'),
+            clarity=request.form.get('clarity')
         )
-        final_data=data.get_data_as_dataframe()
+        final_data = data.get_data_as_dataframe()
+        predict_pipeline = PredictPipeline()
+        pred = predict_pipeline.predict(final_data)
+        result = round(pred[0], 2)
 
-        predict_pipeline=PredictPipeline()
+        return render_template("result.html", final_result=result, model_score=round(ModelTrainer().initate_model_trainin()*100, 2))
+    
 
-        pred=predict_pipeline.predict(final_data)
-
-        result=round(pred[0],2)
-
-        return render_template("result.html",final_result=result)
-
-
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
